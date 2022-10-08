ee52c970cfd512aacea7dd300198f36478a1d1c6
Elena <lnlnlne@yandex.ru>
2022-10-08 17:44:23 +0500

    end

--- database.py
+++ database.py
@@ -3,17 +3,20 @@ import sqlite3
 connect = sqlite3.connect('C:/Users/lnlnl/Desktop/bot/db/dbForBot.db', check_same_thread=False)
 cursor = connect.cursor()
 
+
 # ДОБАВЛЕНИЕ ПОЛЬЗОВАТЕЛЯ В БД
 def add_users_in_db(user_id, name_user):
     cursor.execute('INSERT INTO users (user_id, name_user) VALUES (?, ?)', (user_id, name_user))
     connect.commit()
 
+
 # ПОИСК ПОЛЬЗОВАТЕЛЯ В БД
-def exist_User(user_id):
+def exist_user(user_id):
     sql = "SELECT users.user_id FROM users WHERE user_id=:user_id"
-    result = cursor.execute(sql, {'user_id':user_id}).fetchone()
+    result = cursor.execute(sql, {'user_id': user_id}).fetchone()
     connect.commit()
     return result
+
 
 # ВЫВОД ВСЕХ КАТЕГОРИЙ
 def find_categories():
@@ -21,20 +24,31 @@ def find_categories():
     connect.commit()
     return result
 
+
 # ПОДПИСКА ПОЛЬЗОВАТЕЛЯ НА КАТЕГОРИЮ
 def add_subscr(user_id, category_name):
     sql = "INSERT INTO subscriptions (user_id, category_name) VALUES (:user_id, :category_name)";
-    cursor.execute(sql, {'user_id':user_id, 'category_name':category_name})
+    cursor.execute(sql, {'user_id': user_id, 'category_name': category_name})
     connect.commit()
 
+
+# ПОИСК КАТЕГОРИИ ПО ПОЛЬЗОВАТЕЛЮ
 def find_subscr(user_id):
     sql = "SELECT subscriptions.category_name FROM subscriptions WHERE user_id = :user_id"
-    result = cursor.execute(sql, {'user_id':user_id}).fetchall()
+    result = cursor.execute(sql, {'user_id': user_id}).fetchall()
     connect.commit()
     return result
 
-def findIdCat(user_id, category_name):
+
+# ПОИСК КАТЕГОРИИ
+def find_id_cat(user_id, category_name):
     sql = "SELECT subscriptions.category_name FROM subscriptions WHERE user_id = :user_id AND category_name =:category_name"
-    result = cursor.execute(sql, {'user_id':user_id,'category_name':category_name}).fetchone();
+    result = cursor.execute(sql, {'user_id': user_id, 'category_name': category_name}).fetchone();
     connect.commit();
     return result;
+
+
+def unsub_cat(user_id, category_name):
+    sql = "DELETE FROM subscriptions WHERE user_id=:user_id and category_name =:category_name";
+    cursor.execute(sql, {'user_id': user_id, 'category_name': category_name});
+    connect.commit();
--- main.py
+++ main.py
@@ -8,9 +8,9 @@ bot = telebot.TeleBot(config.tken_bot)
 
 apiKey = "9005b1597fff4141a27edf5fa19e3112"
 
-news = []
 country = "ru"
 category = "technology"
+
 
 # ФУНКЦИИ
 def convert_list(new):
@@ -19,19 +19,43 @@ def convert_list(new):
         str += i + "\n"
     return str
 
-def sub(user_id, category):
-    if (database.findIdCat(user_id, category.data) == None):
-        database.add_subscr(category.from_user.id, category.data)
-        bot.answer_callback_query(category.id, text="Вы успешно подписались!")
+
+def sub(user_id, category, call):
+    if (database.find_id_cat(user_id, category) == None):
+        database.add_subscr(call.from_user.id, category)
+        bot.answer_callback_query(call.id, text="Вы успешно подписались!")
     else:
-        bot.answer_callback_query(category.id, show_alert=True, text="Вы уже подписаны на эту категорию.")
+        bot.answer_callback_query(call.id, show_alert=True, text="Вы уже подписаны на эту категорию.")
+
+
+def get_news(category):
+    a = requests.get(
+        f'https://newsapi.org/v2/top-headlines/?apiKey={apiKey}&country={country}&category={category}&pageSize=2')
+    news = []
+    for i in a.json()['articles']:
+        news.append([i['title'], i['url']])
+    answer = ""
+    for line in news:
+        answer += convert_list(line) + "\n"
+    return answer
+
+
+def btn_redraw(call, user_id):
+    bot.answer_callback_query(call.id, text="Вы успешно отписались!")
+    bot.delete_message(call.message.chat.id, call.message.message_id)
+    cats = database.find_subscr(user_id)
+    markup = types.InlineKeyboardMarkup()
+    for item in cats:
+        markup.add(types.InlineKeyboardButton(item[0], callback_data=f'unsub-{item[0]}'))
+    bot.send_message(call.message.chat.id, "Категории, на которые вы подписаны:", reply_markup=markup)
+
 
 @bot.message_handler(commands=['start'])
 def send_start(message):
     bot.reply_to(message, "Добро пожаловать в MooreNews, " + message.from_user.first_name + "!")
     user_id = message.from_user.id
     user_name = message.from_user.first_name
-    exist_us = database.exist_User(user_id);
+    exist_us = database.exist_user(user_id);
     if (exist_us == None):
         database.add_users_in_db(user_id, user_name)
     # КНОПКИ
@@ -46,20 +70,22 @@ def send_start(message):
         markup.add(subscription_management)
         bot.send_message(message.chat.id, "Доступные функции:", reply_markup=markup)
 
+
 # РАБОТА С КНОПКАМИ
 @bot.message_handler(content_types=['text'])
 def send_news(message):
     user_id = message.from_user.id
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    category = database.find_subscr(user_id)
+
     # ПОКАЗАТЬ НОВОСТИ
-    if(message.text == "Показать новости"):
-        a = requests.get(f'https://newsapi.org/v2/top-headlines/?apiKey={apiKey}&country={country}&category={category}&pageSize=2')
-        for i in a.json()['articles']:
-            news.append([i['title'], i['url']])
-        answer = ""
-        for line in news:
-            answer += convert_list(line) + "\n"
-        bot.send_message(message.chat.id, answer)
+    if (message.text == "Показать новости"):
+        cats = database.find_subscr(user_id)
+        markup = types.InlineKeyboardMarkup()
+        for item in cats:
+            markup.add(types.InlineKeyboardButton(item[0], callback_data=f'news-{item[0]}'))
+        bot.send_message(message.chat.id, "Доступные категории:", reply_markup=markup)
+
     # УПРАВЛЕНИЕ ПОДПИСКАМИ
     elif (message.text == "Управление подписками"):
         my_subscription = types.KeyboardButton('Мои подписки')
@@ -83,17 +109,14 @@ def send_news(message):
         cats = database.find_categories()
         markup = types.InlineKeyboardMarkup()
         for item in cats:
-            btn = item[1]
-            markup.add(types.InlineKeyboardButton(btn, callback_data=btn))
+            markup.add(types.InlineKeyboardButton(item[1], callback_data=f'sub-{item[1]}'))
         bot.send_message(message.chat.id, "Доступные категории:", reply_markup=markup)
     # ОТПИСАТЬСЯ
     elif (message.text == "Отписаться"):
         cats = database.find_subscr(user_id)
         markup = types.InlineKeyboardMarkup()
         for item in cats:
-            btn2 = item[0]
-            markup.add(types.InlineKeyboardButton(btn2, callback_data=btn2))
-            print(btn2)
+            markup.add(types.InlineKeyboardButton(item[0], callback_data=f'unsub-{item[0]}'))
         bot.send_message(message.chat.id, "Категории, на которые вы подписаны:", reply_markup=markup)
     # НАЗАД
     elif (message.text == "Назад"):
@@ -102,25 +125,61 @@ def send_news(message):
         markup.add(show_news, subscription_management)
         bot.send_message(message.chat.id, "Доступные функции:", reply_markup=markup)
 
+
 # КНОПКИ С ПОДПИСКАМИ
 @bot.callback_query_handler(func=lambda call: True)
 def send_text(call):
+    command = call.data.split('-')[0]
+    data = call.data.split('-')[1]
     user_id = call.from_user.id
-    if call.data == 'science':
-        sub(user_id, call)
-    elif call.data == 'business':
-        sub(user_id, call)
-    elif call.data == 'health':
-        sub(user_id, call)
-    elif call.data == 'general':
-        sub(user_id, call)
-    elif call.data == 'technology':
-        sub(user_id, call)
-    elif call.data == 'sports':
-        sub(user_id, call)
 
+    if (command == 'sub'):
+        if data == 'science':
+            sub(user_id, data, call)
+        elif data == 'business':
+            sub(user_id, data, call)
+        elif data == 'health':
+            sub(user_id, data, call)
+        elif data == 'general':
+            sub(user_id, data, call)
+        elif data == 'technology':
+            sub(user_id, data, call)
+        elif data == 'sports':
+            sub(user_id, data, call)
 
+    elif (command == 'unsub'):
+        if data == 'science':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
+        elif data == 'business':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
+        elif data == 'health':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
+        elif data == 'general':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
+        elif data == 'technology':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
+        elif data == 'sports':
+            database.unsub_cat(user_id, data)
+            btn_redraw(call, user_id)
 
+    elif (command == 'news'):
+        if data == 'science':
+            bot.send_message(call.message.chat.id, get_news(data))
+        elif data == 'business':
+            bot.send_message(call.message.chat.id, get_news(data))
+        elif data == 'health':
+            bot.send_message(call.message.chat.id, get_news(data))
+        elif data == 'general':
+            bot.send_message(call.message.chat.id, get_news(data))
+        elif data == 'technology':
+            bot.send_message(call.message.chat.id, get_news(data))
+        elif data == 'sports':
+            bot.send_message(call.message.chat.id, get_news(data))
 
 
 bot.polling()
